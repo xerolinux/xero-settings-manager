dbc70ae12fc27fe79f73b469426a0968ddc645ea
DarkXero-dev <steve@techxero.com>
2023-08-16 21:03:50 +0300

    freak

--- src/msm/main.cpp
+++ src/msm/main.cpp
@@ -38,11 +38,6 @@ int main( int argc, char* argv[] )
 
     // Needed for resources in libs
     Q_INIT_RESOURCE( kernel );
-    Q_INIT_RESOURCE( keyboard );
-    Q_INIT_RESOURCE( language_packages );
-    Q_INIT_RESOURCE( timedate );
-    Q_INIT_RESOURCE( translations );
-    Q_INIT_RESOURCE( users );
 
     QCommandLineParser parser;
     parser.setApplicationDescription( app.applicationName() );
