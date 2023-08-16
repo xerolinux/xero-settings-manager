b8ec32942a1fed43c24b48d7b94d79f27614592e
DarkXero-dev <steve@techxero.com>
2023-08-16 16:22:27 +0300

    Xero

--- src/msm/MsmApplication.cpp
+++ src/msm/MsmApplication.cpp
@@ -1,21 +1,21 @@
 /*
- *  This file is part of Garuda Settings Manager.
+ *  This file is part of XeroLinux Settings Manager.
  *
  *  Ramon Buldó <ramon@manjaro.org>
  *  Kacper Piwiński
  *
- *  Garuda Settings Manager is free software: you can redistribute it and/or modify
+ *  XeroLinux Settings Manager is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  Garuda Settings Manager is distributed in the hope that it will be useful,
+ *  XeroLinux Settings Manager is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with Garuda Settings Manager.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with XeroLinux Settings Manager.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "MsmApplication.h"
@@ -29,16 +29,16 @@ MsmApplication::MsmApplication( int& argc, char* argv[] )
     : QApplication( argc, argv ),
       m_mainwindow( 0 )
 {
-    setOrganizationName( "Garuda" );
-    setOrganizationDomain( "Garuda" );
-    setApplicationName( "Garuda Settings Manager" );
+    setOrganizationName( "XeroLinux" );
+    setOrganizationDomain( "XeroLinux" );
+    setApplicationName( "XeroLinux Settings Manager" );
     setApplicationVersion( "0.5.5" );
 }
 
 
 MsmApplication::~MsmApplication()
 {
-    qDebug() << "Shutting down Garuda Settings Manager...";
+    qDebug() << "Shutting down XeroLinux Settings Manager...";
 }
 
 
